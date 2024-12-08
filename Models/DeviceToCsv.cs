a7c7ae89d98b704bdeb6d6e109caae375ff9f800
emeryta <osi85m@hotmail.com>
2024-12-08 09:44:51 +0100

    Agregar archivos de proyecto.

---
+++ Models/DeviceToCsv.cs
@@ -0,0 +1,63 @@
+﻿namespace GeoTab.Models
+{
+    internal class DeviceToCsv
+    {
+        public string Id = String.Empty;
+        public string Name = String.Empty;
+
+        public string Coordinates = String.Empty;
+        public string Odometer = String.Empty;
+        public string VIN = String.Empty;
+        public string TimeStamp = String.Empty;
+
+        public void WriteDataToCsv()
+        {
+            string csvFileName = "DeviceBackup - " + Name+".csv";
+            string csvPath = "GeoTabBackups";
+            string downloadFolderPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\" + csvPath;
+
+            if (!Directory.Exists(downloadFolderPath))
+            {
+                try
+                {
+                    System.IO.Directory.CreateDirectory(downloadFolderPath);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"An error occurred while creating backup folder: {ex.Message}");
+                }
+            }
+
+            string downloadFilePath = Path.Combine(downloadFolderPath, Path.GetFileName(csvFileName));
+            if (!File.Exists(downloadFilePath))
+            {
+                try
+                {
+                    using (StreamWriter writer = new StreamWriter(downloadFilePath))
+                    {
+                        writer.WriteLine("Id, Timestamp, VIN, Coordinates, Odometer");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"An error occurred while writing the header to the file: {ex.Message}");
+                }
+            }
+
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(downloadFilePath, append: true))
+                {
+                    writer.WriteLine($"{Id}, {TimeStamp}, {VIN}, {Coordinates}, {Odometer}");
+
+                }
+
+                Console.WriteLine(TimeStamp + " Data written to CSV file. - " + Id);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred while writing to the file: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,103 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Geotab.Checkmate.ObjectModel;
+using Geotab.Checkmate;
+using GeoTab.Models;
+using Geotab.Checkmate.ObjectModel.Engine;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        var timer = new PeriodicTimer(TimeSpan.FromMinutes(1));
+        Console.WriteLine("Waiting for next iteration...");
+
+        while (await timer.WaitForNextTickAsync())
+        {
+            #region process
+            Console.WriteLine("Iterate " + DateTime.Now.ToString("yyyyMMdd_HHmmssffff"));
+
+            var api = new API("maria.rius08@gmail.com", "geotabpass", null, "demo_candidates_net", "mypreview.geotab.com");
+            await api.AuthenticateAsync();
+
+            try
+            {
+                IList<Device> devices = await api.CallAsync<IList<Device>>("Get", typeof(Device), new { search = new DeviceSearch { Groups = new List<GroupSearch> { new GroupSearch(KnownId.GroupVehicleId) } } }) ?? new List<Device>();
+                foreach (var device in devices)
+                {
+                    DeviceToCsv deviceToCsv = new DeviceToCsv()
+                    {
+                        Id = device.Id?.ToString() ?? String.Empty,
+                        Name = device.Name ?? String.Empty,
+                        TimeStamp = DateTime.Now.ToString("yyyyMMdd_HHmmssffff")
+                    };
+
+                    #region VIN
+                    try
+                    {
+                        deviceToCsv.VIN = ((XDevice)device).VehicleIdentificationNumber ?? string.Empty;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"An error occurred while retrieving VIN: {ex.Message}");
+                    }
+                    #endregion
+
+                    #region odometer
+                    try
+                    {
+                        IList<StatusData> statusData = await api.CallAsync<IList<StatusData>>("Get", typeof(StatusData), new
+                        {
+                            search = new StatusDataSearch
+                            {
+                                DeviceSearch = new DeviceSearch(device.Id),
+                                DiagnosticSearch = new DiagnosticSearch(KnownId.DiagnosticOdometerAdjustmentId),
+                                FromDate = DateTime.MaxValue
+                            }
+                        });
+                        deviceToCsv.Odometer = Math.Truncate((double)statusData[0].Data / 1000).ToString();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"An error occurred while retrieving Odometer: {ex.Message}");
+                    }
+                    #endregion
+
+                    #region coordinates
+                    try
+                    {
+                        IList<DeviceStatusInfo> deviceStatusInfo = await api.CallAsync<List<DeviceStatusInfo>>("Get", typeof(DeviceStatusInfo), new
+                        {
+                            search = new DeviceStatusInfoSearch
+                            {
+                                DeviceSearch = new DeviceSearch
+                                {
+                                    Id = device.Id
+                                }
+                            }
+                        });
+                        deviceToCsv.Coordinates = "Lng " + deviceStatusInfo[0].Longitude.ToString().Replace(',', '.') + " Lat " + deviceStatusInfo[0].Latitude.ToString().Replace(',', '.');
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"An error occurred while retrieving Coordinates: {ex.Message}");
+                    }
+                    #endregion
+
+                    try
+                    {
+                        deviceToCsv.WriteDataToCsv();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"An error occurred while downloading the file: {ex.Message}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+        }
+        #endregion
+    }
+}
